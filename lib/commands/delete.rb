395228d6183667f47792155f7b8b724ac157ee61
jstaffans <johannes.staffans@gmail.com>
2014-11-20 17:58:51 +0100

    Fix indentation

--- lib/commands/delete.rb
+++ lib/commands/delete.rb
@@ -6,10 +6,10 @@ def delete(ops, stack_name, input)
   existing_stack = ops.find_stack stack_name
   if existing_stack.nil? == false
     if input.choice("Delete stack with the name #{existing_stack.stack_name()}", "Yn") == 'y'
-    	existing_stack.delete
-	end
+      existing_stack.delete
+  end
   else
-  	puts "stack #{stack_name} does not exist"
-  	exit -1
+    puts "stack #{stack_name} does not exist"
+    exit -1
   end
 end
