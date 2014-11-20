c5589bca2ba6c70c8b8540da49b05e12c10122e8
jstaffans <johannes.staffans@gmail.com>
2014-11-20 18:00:45 +0100

    Fix indentation

--- lib/vagrant/vagrant.rb
+++ lib/vagrant/vagrant.rb
@@ -15,61 +15,61 @@ def vagrant_test(config)
   stack_name = config['stack']['name']
   layers = config['layers']
   layers.each do |l|
-  	layer_config = l['config']
-  	instance = l['instances'].first
+    layer_config = l['config']
+    instance = l['instances'].first
 
-  	test_directory = "/tmp/#{stack_name}/#{layer_config['shortname']}/"
-  	prepare_directory(test_directory)
-  	puts "testing #{stack_name} :: #{layer_config['shortname']} in #{test_directory}"
+    test_directory = "/tmp/#{stack_name}/#{layer_config['shortname']}/"
+    prepare_directory(test_directory)
+    puts "testing #{stack_name} :: #{layer_config['shortname']} in #{test_directory}"
 
-  	# we want to run all setup and deploy recipes
-  	vagrant_config = {:name => "#{layer_config['shortname']}"}
-  	stack_configuration = {
-  		:opsworks => {
-  			:stack => {
-  				:name => stack_name
-  			},
-  			:instance => {
-  				:layers => [layer_config['shortname']]
-  			}
-  		}
-  	}
-  	
-  	if config['stack'].has_key? 'custom_json'
-  		stack_configuration = stack_configuration.merge(config['stack']['custom_json'])
-  	end
+    # we want to run all setup and deploy recipes
+    vagrant_config = {:name => "#{layer_config['shortname']}"}
+    stack_configuration = {
+      :opsworks => {
+        :stack => {
+          :name => stack_name
+        },
+        :instance => {
+          :layers => [layer_config['shortname']]
+        }
+      }
+    }
+    
+    if config['stack'].has_key? 'custom_json'
+      stack_configuration = stack_configuration.merge(config['stack']['custom_json'])
+    end
 
-  	# set up iam role - extract last part of arn
-  	role_arn = config['stack']['default_instance_profile_arn']
-  	puts "Creating temporary credentials for instance role " + role_arn[/\/(.*)/,1]
+    # set up iam role - extract last part of arn
+    role_arn = config['stack']['default_instance_profile_arn']
+    puts "Creating temporary credentials for instance role " + role_arn[/\/(.*)/,1]
 
-  	assumed_role = AWS::STS.new.assume_role({:role_session_name => 'temporary', :role_arn => role_arn.sub!('instance-profile', 'role')})
-  	stack_configuration[:aws] = {
-  		:region => config['stack']['region'],
-  		:access_key_id => assumed_role[:credentials][:access_key_id],
-  		:secret_access_key => assumed_role[:credentials][:secret_access_key],
-  		:session_token => assumed_role[:credentials][:session_token]
-  	}
+    assumed_role = AWS::STS.new.assume_role({:role_session_name => 'temporary', :role_arn => role_arn.sub!('instance-profile', 'role')})
+    stack_configuration[:aws] = {
+      :region => config['stack']['region'],
+      :access_key_id => assumed_role[:credentials][:access_key_id],
+      :secret_access_key => assumed_role[:credentials][:secret_access_key],
+      :session_token => assumed_role[:credentials][:session_token]
+    }
 
-  	vagrant_config[:custom_json] = stack_configuration.inspect
-  	if layer_config.has_key? 'custom_recipes'
-  		recipes = layer_config['custom_recipes']['setup'] + layer_config['custom_recipes']['deploy']
-  	  	vagrant_config[:run_list] = recipes.map{|x| "\"" + x + "\"" }.join(',')
-  	end
-  	save_config(vagrant_config, test_directory + "Vagrantfile")
+    vagrant_config[:custom_json] = stack_configuration.inspect
+    if layer_config.has_key? 'custom_recipes'
+      recipes = layer_config['custom_recipes']['setup'] + layer_config['custom_recipes']['deploy']
+        vagrant_config[:run_list] = recipes.map{|x| "\"" + x + "\"" }.join(',')
+    end
+    save_config(vagrant_config, test_directory + "Vagrantfile")
 
-  	download_archive(config['stack']['custom_cookbooks_source'], test_directory)
+    download_archive(config['stack']['custom_cookbooks_source'], test_directory)
 
-  	system("cd #{test_directory} && vagrant up && vagrant destroy -f")
+    system("cd #{test_directory} && vagrant up && vagrant destroy -f")
   end
 end
 
 # prepares a clean directory
 def prepare_directory(name)
-	if Dir.exists? name
-		FileUtils.rm_rf name
-	end
-  	FileUtils.mkdir_p name
+  if Dir.exists? name
+    FileUtils.rm_rf name
+  end
+    FileUtils.mkdir_p name
 end
 
 
@@ -85,21 +85,21 @@ def save_config(config, target)
 end
 
 def download_archive(cookbooks_source, target)
-	if cookbooks_source['type'] == 's3'
-		url = cookbooks_source['url']
-		url = url.gsub(/^http.:\/\/.*amazonaws.com\//,'s3://')
-		# TODO how to ret rid of the region
-		puts_system("aws s3 cp --region eu-west-1 #{url} #{target}archive.tar.gz")
-		puts_system("cd #{target} && tar -xzf archive.tar.gz && mv cookbooks/* .")
-	else
-		puts "download source type #{cookbooks_source['type']} not yet supported"
-		exit 1
-	end
+  if cookbooks_source['type'] == 's3'
+    url = cookbooks_source['url']
+    url = url.gsub(/^http.:\/\/.*amazonaws.com\//,'s3://')
+    # TODO how to ret rid of the region
+    puts_system("aws s3 cp --region eu-west-1 #{url} #{target}archive.tar.gz")
+    puts_system("cd #{target} && tar -xzf archive.tar.gz && mv cookbooks/* .")
+  else
+    puts "download source type #{cookbooks_source['type']} not yet supported"
+    exit 1
+  end
 end
 
 def puts_system(cmd)
-	puts "> #{cmd}"
-	if not system cmd
-		exit 1
-	end
+  puts "> #{cmd}"
+  if not system cmd
+    exit 1
+  end
 end
