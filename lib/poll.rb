5e815dd45cc2161d1e473eeccfc9b1102b30c3f7
jstaffans <johannes.staffans@gmail.com>
2014-11-27 19:10:54 +0100

    Include copy of gem

---
+++ lib/poll.rb
@@ -0,0 +1,28 @@
+# A module for polling that supports timeouts. 
+# 
+# @example
+#   Poll.poll do
+#     instances = <get instances>
+#     all_instances_have_status? instances, "online"
+#   end
+#  
+
+module Poll
+
+  def poll(seconds=nil, delay=15) 
+    seconds ||= 10*60 
+    give_up_at = Time.now + seconds 
+    failure = nil  
+
+    while Time.now < give_up_at do
+      result = yield
+      return result if result
+      sleep delay
+    end
+    msg ||= "polling failed after #{seconds} seconds" 
+    raise msg
+  end
+
+  module_function :poll
+
+end
