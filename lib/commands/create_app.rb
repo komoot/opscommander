4939a263727e3f93f1d10d63122a57ef0a0ca05c
jstaffans <johannes.staffans@gmail.com>
2014-12-02 15:48:08 +0100

    Use generic template variables in all commands. Closes #3

--- lib/commands/create_app.rb
+++ lib/commands/create_app.rb
@@ -1,20 +1,7 @@
-def create_app(aws_connection, configuration, app_name, variables)
+def create_app(aws_connection, configuration, app_name)
   ops = OpsWorks.new(aws_connection)
   stack_name = configuration['stack']['name']
   stack = ops.find_stack(stack_name)
-  config = override_configuration(configuration['apps'][app_name], variables)
-  stack.create_app(app_name, config)
+  stack.create_app(app_name, configuration)
 end
 
-def override_configuration(app_config, variables)
-  if not variables or variables.empty? 
-    return app_config
-  end
-
-  variables.split(',').each do |v|
-    parts = v.strip.split('=')
-    app_config['environment'][parts[0]] = parts[1]
-  end
-
-  return app_config
-end
