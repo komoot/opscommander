36e741a140187afb052fae28d3be7d780b161b2b
jstaffans <johannes.staffans@gmail.com>
2014-12-03 17:37:55 +0100

    Test for bluegreen

---
+++ spec/lib/commands/bluegreen_spec.rb
@@ -0,0 +1,32 @@
+require 'commands/bluegreen'
+
+describe 'bluegreen' do
+  
+  context 'without explicit ELB configuration' do
+    before(:each) do
+      @config = {
+        'stack' => {'name' => 'Test stack'},
+        'layers' => [{
+          'config' => {'name' => 'Test layer'},
+          'elb' => nil
+        }]
+      }
+
+      @stack = double('stack')
+
+      @ops = double('ops')
+      allow(@ops).to receive(:find_stack).with('Test stack-blue').and_return(false)
+      allow(@ops).to receive(:find_stack).with('Test stack-green').and_return(false)
+      allow(@ops).to receive(:find_stack).with('Test stack').and_return(@stack)
+    end
+      
+    it 'Should fail if no manually configured ELB is found' do
+      expect(@stack).to receive(:supports_bluegreen_deployment?).and_return(false)
+      expect(@stack).to_not receive(:rename_to)
+      expect { bluegreen(@ops, @config, nil) }.to raise_error(RuntimeError, /doesn't support blue-green deployment/)
+    end
+
+  end
+
+end
+
